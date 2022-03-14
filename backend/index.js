99035f085a0ad251a4fc3e4e329622a44bcee595
danielkingstonweb <87955791+danielkingstonweb@users.noreply.github.com>
2022-03-14 20:18:47 +1300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+    // Installing packages begins
+const express = require('express'); //Includes express js
+const app = express(); // Variable for express
+const bodyParser = require('body-parser'); // Includes bodyParser
+const mongoose = require('mongoose'); // Includes mongoose
+const bcrypt = require('bcryptjs'); // Includes bcrypt
+const cors = require('cors'); // Includes cors
+const config = require('./config.json');
+
+    // Backend port number
+const port = 5000;
+
+
+
+app.use(bodyParser.json()); // Calling the body parser method
+app.use(bodyParser.urlencoded({extended:false})); // Preventing the URL from being parsed
+
+app.use(cors()); // Calling cors method with express
+
+app.get('/', (req,res) => res.send('Hello! I am from the backend baybeeeeee'))
+
+
+mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.${config.MONGO_CLUSTER_NAME}.mongodb.net${config.MONGO_DBNAME}?retryWrites=true&w=majority`, {useNewUrlParser:true,useUnifiedTopology:true}).then(()=>console.log('DB Connected')).catch(err=>{
+    console.log(`DB Connection Error ${err.message}`);
+});
