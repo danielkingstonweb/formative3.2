fa266f372970ac2052c5f10054ed2d75c26bc305
rory <turnburory@gmail.com>
2022-03-15 14:54:00 +1300

    coding style conventions added

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-    // Installing packages begins
+// <--! Global backend constants start here -->
+   // Installing packages begins
 const express = require('express'); //Includes express js
 const app = express(); // Variable for express
 const bodyParser = require('body-parser'); // Includes bodyParser
@@ -15,7 +16,9 @@ const Portfolios = require('./models/portfolios');
 // Backend port number
 const port = 5000;
 
+// <--! Global backend constants end here -->
 
+// <--! App parser code starts here -->
 
 app.use(bodyParser.json()); // Calling the body parser method
 app.use(bodyParser.urlencoded({extended:false})); // Preventing the URL from being parsed
@@ -24,8 +27,11 @@ app.use(cors()); // Calling cors method with express
 
 app.get('/', (req,res) => res.send('Hello! I am from the backend baybeeeeee'))
 
+// <--! App parser code ends  here -->
 
+// <--! Mongoose url code starts here -->
 mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@teamWork.${config.MONGO_CLUSTER_NAME}.mongodb.net/${config.MONGO_DBNAME}?retryWrites=true&w=majority`, {useNewUrlParser:true,useUnifiedTopology: true}).then(()=>console.log('DB Connected!'))
 .catch(err=>{
   console.log(`DB Connection Error:${err.message}`); 
 });
+// <--! Mongoose url code ends here -->
